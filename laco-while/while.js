a7581a7c484824af0144faa873a0bfc1e9bd51e5
Geovane-note <geovane.ita@gmail.com>
2023-05-28 06:06:49 -0300

    aula de repetição

---
+++ laco-while/while.js
@@ -0,0 +1,10 @@
+let listaCompras = ['Uva','Banana','Abacate'];
+
+let i = 0;
+
+while (i < listaCompras){
+    console.table(listaCompras[i])
+    i ++;
+}
+
+console.table(listaCompras);
--- todo/todo.js
+++ todo/todo.js
@@ -1,38 +1,47 @@
 let todoList = [];
 
-function addItem(title){
-    
+function addItem(title)
+{    
     todoList.push({
         title : title,
         done  : false
     });
 }
 
-function refreshui(){
-    
+function refreshUI()
+{    
     let htmlData = '';
+    let i = 0;
 
-    for(var i=0; i < todoList.length; i++){
+   // for(var i = 0; i < todoList.length; i++)
+   while (i < todoList.length)
+    {
+       htmlData += `
+          <div>
+            <label>
+                <input type = "checkbox" name= "done" value = "${todoList[i].done}" />
+                <span style= "${todoList[i].done ? 'text-decoration: line-through' : ''}" >
+                ${todoList[i].title}
+                </span>
+            </label>
+          </div>       
+          `;            
 
-       // if(!todoList[i].done){
-            htmlData += `
-            <div>
-                <label>
-                     <input type = "checkbox" name= "done" value = "${todoList[i].done}" />
-                     <span sttyle= "${todoList[i].done ? 'text-decoration: line-through' : ''}" >
-                     ${todoList[i].title}
-                     </span>
-                </label>
-            </div>       
-            `;
-       // }       
+          i++;
     }
 
     document.getElementById('todoList').innerHTML = htmlData;
 }
 
+function addItemClickHandler()
+{
+    addItem(document.getElementById('todo').value);
+    refreshUI();
+    document.getElementById('todo').value = '';
+}
+
 addItem('Criar pagina HTML para exemplo de Lista de Tarefas')
 
 window.addEventListener('load', function(){
-    refreshui();
+    refreshUI();
 });
